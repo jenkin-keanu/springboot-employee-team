4189ea3c141d3739d2e2ff5b2b2bad1506bd08b6
jenkin <jenkincs@163.com>
2020-07-28 23:10:07 +0800

    [springboot employee][Jenkin] feature: pass findCompaniesByCondition(@RequestParam(required = false, defaultValue = -1) int page,

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -6,6 +6,7 @@ import com.thoughtworks.springbootemployee.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,7 +18,12 @@ public class CompanyController {
     public List<Company> findAllCompanies(){
         return companyService.findAllCompanies();
     }
-
+    @GetMapping("/companies")
+    public List<Company> findCompaniesByCondition(@RequestParam(required = false, defaultValue = "-1") int page,
+                                                   @RequestParam(required = false, defaultValue = "-1") int pageSize) {
+        if (page != -1) return getPagedCompanies(page, pageSize);
+        return companyService.findAllCompanies();
+    }
     @GetMapping("/companies/{companyId}")
     public List<Employee> findAllEmployeesInCompany(@PathVariable int companyId){
         return companyService.findAllEmployeesInCompany(companyId);
@@ -40,5 +46,17 @@ public class CompanyController {
     @DeleteMapping("/companies/{companyId}")
     public void deleteEmployeeById(@PathVariable int companyId){
         companyService.deleteCompanyById(companyId);
+    }
+
+    private List<Company> getPagedCompanies(int page, int pageSize) {
+        List<Company> companies = companyService.findAllCompanies();
+        List<Company> pagedCompanies = new ArrayList<>();
+
+        int startRow = (page - 1) * pageSize;
+        int endRow = startRow + pageSize;
+        for (int index = startRow; index < companies.size() && index < endRow; index++) {
+            pagedCompanies.add(companies.get(index));
+        }
+        return pagedCompanies;
     }
 }
