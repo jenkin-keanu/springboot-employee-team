1509a61634faae5fcc980a4b9f5af6def0687faf
Keanu.Wang <386247915@qq.com>
2020-07-31 19:44:03 +0800

    [IntegrationTest][Jenkin&Keanu] test: test should_return_is_created_when_save_a_company_given_a_company

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -4,7 +4,9 @@ import com.thoughtworks.springbootemployee.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Integer> {
-
+    List<Company> findByName(String name);
 }
--- src/test/java/com/thoughtworks/springbootemployee/integrationtest/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationtest/CompanyIntegrationTest.java
@@ -5,15 +5,20 @@ import com.thoughtworks.springbootemployee.entity.Company;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import com.thoughtworks.springbootemployee.service.CompanyService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.web.JsonPath;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -46,5 +51,15 @@ public class CompanyIntegrationTest {
         mockMvc.perform(MockMvcRequestBuilders.
                 get("/companies/"+savedCompany.getCompanyId())).
                 andExpect(status().isOk()).andExpect(jsonPath("name").value("jenkin"));
+    }
+
+    @Test
+    void should_return_is_created_when_save_a_company_given_a_company() throws Exception {
+        String companyJson="{\n" +
+                "\t\"name\":\"cargoSmart\"\n" +
+                "}";
+        mockMvc.perform(post("/companies").contentType(MediaType.APPLICATION_JSON).content(companyJson)).andExpect(status().isCreated());
+        List<Company> companies=companyRepository.findByName("cargoSmart");
+        Assertions.assertEquals(1,companies.size());
     }
 }
